95c54b04e397d4d0c45acd17cb1bad354fe78116
tch <ambi.caprice@gmail.com>
2018-03-18 11:47:20 -0700

    test

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -154,7 +154,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.H_ = Hj_;	
 	ekf_.R_  = R_radar_;
 
-    //ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
 	ekf_.H_ = H_laser_;
